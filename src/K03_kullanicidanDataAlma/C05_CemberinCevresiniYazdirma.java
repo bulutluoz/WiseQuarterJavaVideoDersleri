526b8ca210275d3e1654e16c3b8e808132e6434d
bulutluoz <abbulutluoz@gmail.com>
2024-09-03 12:09:29 +0200

    K03 sonu sonu

--- src/K03_kullanicidanDataAlma/C05_CemberinCevresiniYazdirma.java
+++ src/K03_kullanicidanDataAlma/C05_CemberinCevresiniYazdirma.java
@@ -1,5 +1,7 @@
 package K03_kullanicidanDataAlma;
 
+import java.util.Scanner;
+
 public class C05_CemberinCevresiniYazdirma {
 
     public static void main(String[] args) {
@@ -7,5 +9,15 @@ public class C05_CemberinCevresiniYazdirma {
         // Soru 6- Kullanicidan bir cemberin yaricapini alip,
         //         cevresini ve alanini yazdirin.
         //         cevre = 2* PI * yaricap , alan = PI * yaricap * yaricap
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen yaricapi cm olarak giriniz..");
+        double yaricap = scanner.nextDouble();
+
+        System.out.println( "Cemberin cevresi : " + (2* 3.14 * yaricap)  );
+        System.out.println(  "dairenin alani : " + (3.14 * yaricap * yaricap) );
+
+
+
     }
 }
--- src/K03_kullanicidanDataAlma/C06_Swap.java
+++ src/K03_kullanicidanDataAlma/C06_Swap.java
@@ -1,8 +1,29 @@
 package K03_kullanicidanDataAlma;
 
+import java.util.Scanner;
+
 public class C06_Swap {
     public static void main(String[] args) {
 
-        // Soru 7 (Interview)- Kullanicidan iki sayi alip ikisinin degerlerini degistirin(swap).
+        // Soru 7 (Interview)- Kullanicidan iki sayi alip
+        //                     ikisinin degerlerini degistirin(swap).
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Birinci tamsayiyi giriniz...");
+        int sayi1 = scanner.nextInt(); // 25
+
+        System.out.println("Ikinci tamsayiyi giriniz...");
+        int sayi2 = scanner.nextInt(); // 40
+
+        int temp = 0;       // s1=25 s2=40 temp=0
+        temp = sayi2;       // s1=25 s2=40 temp=40
+        sayi2 = sayi1;      // s1=25 s2=25 temp=40
+        sayi1 = temp;       // s1=40 s2=25 temp=40
+
+        System.out.println("sayi1'in yeni degeri : " + sayi1); // 40
+        System.out.println("sayi2'nin yeni degeri : " + sayi2); // 25
+
+
     }
 }

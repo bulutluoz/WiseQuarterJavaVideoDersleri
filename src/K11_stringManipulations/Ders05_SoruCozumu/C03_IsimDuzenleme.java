16b4811200680f80801ed94e4eefd14c9bac845b
bulutluoz <abbulutluoz@gmail.com>
2024-10-04 11:06:57 +0200

    Konu11 sonu

--- src/K11_stringManipulations/Ders05_SoruCozumu/C03_IsimDuzenleme.java
+++ src/K11_stringManipulations/Ders05_SoruCozumu/C03_IsimDuzenleme.java
@@ -21,6 +21,34 @@ public class C03_IsimDuzenleme {
         String soyisim = scanner.nextLine();
 
 
+        if (isim.length() > soyisim.length()){
+            //    isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde yazdirin
+
+            System.out.println(
+
+                    isim.substring(0,1).toUpperCase()+
+                    isim.substring(1).toLowerCase()+
+                    " "+
+                    soyisim.substring(0,1).toUpperCase()+
+                    soyisim.substring(1).toLowerCase()
+            );
+
+
+        }
+
+
+        if (soyisim.length() > isim.length()){
+            //    ismi ilk harf buyuk digerleri kucuk, soyismin tamamini buyuk harflerle yazdirin.
+
+            System.out.println(
+                    isim.substring(0,1).toUpperCase()+
+                    isim.substring(1).toLowerCase()+
+                    " "+
+                    soyisim.toUpperCase()
+            );
+
+        }
+
 
     }
 }
--- src/K11_stringManipulations/Ders05_SoruCozumu/C04_MetinDegistirme.java
+++ src/K11_stringManipulations/Ders05_SoruCozumu/C04_MetinDegistirme.java
@@ -14,6 +14,35 @@ public class C04_MetinDegistirme {
         System.out.println("Lutfen bir metin giriniz...");
         String metin = scanner.nextLine();
 
+        int uzunluk = metin.length();
+
+
+        if ( uzunluk % 2 == 0){ // metnin uzunlugu cift
+            //  String’in uzunlugu cift sayi ise tam ortasina :) ekleyin,
+
+            System.out.println(
+
+                    metin.substring( 0,uzunluk/2 ) +
+                    ":)" +
+                    metin.substring(uzunluk/2)
+
+            );
+
+        }else{ // metnin uzunlugu tek
+            //  String’in uzunlugu tek sayi ise ortadaki harfi silin ve yerine :( yazdirin.
+
+            System.out.println(
+
+                    metin.substring(0,uzunluk/2) +
+                    ":(" +
+                    metin.substring(uzunluk/2+1)
+
+
+            );
+
+
+        }
+
 
 
 

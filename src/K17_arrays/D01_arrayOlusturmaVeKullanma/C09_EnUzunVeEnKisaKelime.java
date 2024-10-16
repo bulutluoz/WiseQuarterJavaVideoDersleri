46d0ce5e9471cf2d479bad88a79af850a56a8ba9
bulutluoz <abbulutluoz@gmail.com>
2024-10-16 16:59:39 +0200

    Konu16 sonu

--- src/K17_arrays/D01_arrayOlusturmaVeKullanma/C09_EnUzunVeEnKisaKelime.java
+++ src/K17_arrays/D01_arrayOlusturmaVeKullanma/C09_EnUzunVeEnKisaKelime.java
@@ -39,6 +39,29 @@ public class C09_EnUzunVeEnKisaKelime {
     // Verilen String bir array’deki
     // en uzun ve en kisa kelimeleri yazdiran bir method olusturun.
 
+    public static void enKisaEnUzunKelimeYazdir(String[] isimler){
+
+        String enUzunKelime = isimler[0];
+        String enKisaKelime = isimler[0];
+
+        for (int i = 0; i < isimler.length ; i++) {
+
+            if ( isimler[i].length() < enKisaKelime.length()){
+                enKisaKelime = isimler[i];
+            }
+
+
+            if (isimler[i].length() > enUzunKelime.length()){
+                enUzunKelime = isimler[i];
+            }
+
+        }
+
+        System.out.println("En uzun kelime : " + enUzunKelime);
+        System.out.println("En kisa kelime : " + enKisaKelime);
+
+    }
+
 
 
 }

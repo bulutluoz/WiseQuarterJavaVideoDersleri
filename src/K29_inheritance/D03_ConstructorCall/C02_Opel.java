6ad8f99aa8c63839b3027411c4e61b462be4fcb0
bulutluoz <abbulutluoz@gmail.com>
2024-11-07 12:40:45 +0100

    Konu29 sonu

---
+++ src/K29_inheritance/D03_ConstructorCall/C02_Opel.java
@@ -0,0 +1,16 @@
+package K29_inheritance.D03_ConstructorCall;
+
+public class C02_Opel extends C01_Car {
+
+    public C02_Opel(){
+        System.out.println("Opel parametresiz constructor calisti");
+    }
+
+    protected String marka = "Opel";
+    protected String motor = "Cevreye duyarli motor";
+    protected String lastik = "Pirelli";
+    protected String guvenlik = "DGS1001";
+    protected String fren = "ABS";
+
+
+}

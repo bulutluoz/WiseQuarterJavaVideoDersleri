7730e4f306d4bb6429594ef355bbd6e9b3809056
bulutluoz <abbulutluoz@gmail.com>
2024-11-24 18:43:55 +0100

    Konu36 sonu

---
+++ src/K36_maps/D06_NestedMapKullanimi/SinifMapDepo.java
@@ -0,0 +1,39 @@
+package K36_maps.D06_NestedMapKullanimi;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class SinifMapDepo {
+
+    public static Map<Integer, Map<String,String> > sinifMap = new HashMap<>();
+
+    static {
+        /*
+
+                sinifMap= { 101={Isim=Ali, soyisim=Can, sinif=10, sube=H, bolum=MF },
+                            102={Isim=Veli, soyisim=Cem, sinif=11, sube=M, bolum=Soz
+                            103={Isim=Ali, soyisim=Cem, sinif=11, sube=H, bolum=TM }
+
+         */
+        // {"Isim"="Ali", "soyisim"="Can", "sinif"="10", "sube"="H", "bolum"="MF" }
+/*
+        sinifMapeOgrenciEkle(102,"Veli","Cem","11","M","Soz");
+        sinifMapeOgrenciEkle(103,"Ali","Cem","11","H","TM");
+        sinifMapeOgrenciEkle(104,"Ayse","Can","10","H","MF");
+        sinifMapeOgrenciEkle(105,"Sevgi","Cem","11","M","TM");
+        sinifMapeOgrenciEkle(106,"Sevgi","Can","10","K","MF");
+        sinifMapeOgrenciEkle(107,"Esra","Han","11","M","SOZ");
+ */
+
+
+
+    }
+
+
+
+
+
+
+
+}
